5fcdbda5db1603a3303d9798c441bf9f93de2da3
Ygor Lausberg <ygor.lausberg@student.uclouvain.be>
2022-11-23 10:30:29 +0100

    Philo

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 
 
 philo: philosophe.c
-	gcc -o prog -Wall main.c
+	gcc -o prog -Wall philosophe.c
 
+
+zip:
+	tar -zcvf prog.tar.gz philosphe.c rw.c prodcons.c Makefile README.md
 
 clean:
 	rm -f src/*.o
--- philosophe.c
+++ philosophe.c
@@ -4,10 +4,6 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#define PHILOSOPHES 3
-
-pthread_t phil[PHILOSOPHES];
-pthread_mutex_t baguette[PHILOSOPHES];
 
 void mange(int id) {
     printf("Philosophe [%d] mange\n",id);
@@ -17,11 +13,12 @@ void pense(int id){
     printf("Philosophe [%d] pense\n",id);
 }
 
-void* philosophe( void* arg ){
+void* philosophe(void* arg){
     int *id=(int *) arg;
     int left = *id;
-    int right = (left + 1) % PHILOSOPHES;
-    while(true){
+    int right = (left + 1) % NB_PHIL;
+    int i = 0;
+    while(i < 100000){
         pense(*id);
         if(left<right){
             pthread_mutex_lock(&baguette[left]);
@@ -33,10 +30,27 @@ void* philosophe( void* arg ){
         mange(*id);
         pthread_mutex_unlock(&baguette[left]);
         pthread_mutex_unlock(&baguette[right]);
+        i++;
     }
     return (NULL);
 }
 
 int main(int argc, char * argv[]){
 
+    if(argc == 1){
+        printf("%s","Please enter number of philosophes");
+        return 0;
+    }
+    int* nb_philosophe = (int*)argv[0];
+
+    pthread_t phil[*nb_philosophe];
+    pthread_mutex_t baguette[*nb_philosophe];
+
+#define NB_PHIL *nb_philosophe
+
+    for(int i = 0; i < *nb_philosophe; i++){
+        if (pthread_create(&phil[i], NULL, &philosophe, (void *) &i) != 0){
+            perror("Creation of worker thread failed.");
+        }
+    }
 }
