5e78bf9c2df913ad8bf6b19647de1d56469c7ba6
Ygor Lausberg <ygor.lausberg@student.uclouvain.be>
2022-11-24 23:03:22 +0100

    Script Philosophe

--- performance.sh
+++ performance.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-echo "Nb Threads,Mesure i,time"
-
 THREADS=(1 2 4 6 8)
-#PROBLEMS=("philosophe" "prodcons" "rw")
 
-#for prob in   "${PROBLEMS[@]}"; do
-  for thread in "${THREADS[@]}"; do
+echo "nb philosophs,nb threads,Mesure i,time" &> measure_philo.csv
+
+NB_PHILOSOPHS=(3 10 100 500)
+for thread in "${THREADS[@]}"; do
+  for nb_phil in "${NB_PHILOSOPHS[@]}"; do
     for i in {1..5}; do
       make philo -j $thread -s
-      /usr/bin/time -f "philosophe,$thread,$i,%E" ./philo $i                      # s = silent (do not print commands in terminal), j = number of jobs(threads)
+      /usr/bin/time -f "$nb_phil,$thread,$i,%E" ./philo $nb_phil >>measure_philo.csv 2>&1                    # s = silent (do not print commands in terminal), j = number of jobs(threads)
       make -s clean
     done
   done
-#done
+done
+
+
 
 #Pour donner permission au script, executer $chmod +x script.sh
-
-# sed "s/ar.*)//"
-
-# ./script.sh &> meausure.csv
