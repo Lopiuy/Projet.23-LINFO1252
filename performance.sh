8f3a45b1e5c671327716921779258ecb19c3a48a
Ygor Lausberg <ygor.lausberg@student.uclouvain.be>
2022-11-25 12:21:49 +0100

    script philo changé (nb_phil = nb_threads) et changer nb_trheads pour tout les autres

--- performance.sh
+++ performance.sh
@@ -2,25 +2,25 @@
 
 # shellcheck disable=SC2016
 
-THREAD_MAX=8    #change in 64 for submission
+THREADS=(1 2 4 8 16 32 64)
 
-echo "nb philosophs,nb threads,Mesure i,time" &> measure_philo.csv
+echo "nb philosophs,Mesure i,time" &> measure_philo.csv
 
-NB_PHILOSOPHS=(3 10 100 500)
-for thread in $(seq 1 $THREAD_MAX); do
-  for nb_phil in "${NB_PHILOSOPHS[@]}"; do
+#NB_PHILOSOPHS=(3 10 100 500)
+#for thread in "${THREADS[@]}"; do
+  for nb_phil in "${THREADS[@]}"; do  #in NB_PHILOSOPHS ?
     for i in {1..5}; do
       make philo -j "$thread" -s
-      /usr/bin/time -f "$nb_phil,$thread,$i,%E" ./philo "$nb_phil" >>measure_philo.csv 2>&1                    # s = silent (do not print commands in terminal), j = number of jobs(threads)
+      /usr/bin/time -f "$nb_phil,$i,%E" ./philo "$nb_phil" >>measure_philo.csv 2>&1                    # s = silent (do not print commands in terminal), j = number of jobs(threads)
       make -s clean
     done
   done
-done
+#done
 
 
 echo "nb_cons,nb_prod,nb threads,Mesure i,time" &> measure_prodcons.csv
 
-for thread in $(seq 2 $THREAD_MAX); do
+for thread in "${THREADS[@]}"; do
   two=2
   c=$((thread / two))
   p=$((thread / two))
@@ -36,7 +36,7 @@ done
 
 echo "nb_reader,nb_writer,nb threads,Mesure i,time" &> measure_rw.csv
 
-for thread in $(seq 2 $THREAD_MAX); do
+for thread in "${THREADS[@]}"; do
   two=2
   r=$((thread / two))
   w=$((thread / two))
